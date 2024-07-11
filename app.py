382774454fd6ee54b490238108666299d88e1d18
Michael Neumann <neumann.michael1993@gmx.de>
2024-07-11 11:44:37 +0200

    Use DictWriter (buggy)

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, Response, request
+from flask import Flask, Response, request, send_file
 import requests
+import io
+import csv
 
 app = Flask(__name__)
 
@@ -25,24 +27,34 @@ def search():
         response = requests.get(api_url, params=params)
     except requests.exceptions.RequestException as e:
         return e, 503
-    data = []
-    data.append(list(response.json().get("results")[0].keys())) # Handle exception when no result is found # Relies on having all keys set on entry 0
-    data[0].remove("openfda")
-    for i,row in enumerate(response.json().get("results")):
-        data.append([])
-        for key_index,key in enumerate(data[0]):
-            data[i+1].append(row.get(key))
-    csv_out = ""
-    for dat in data:
-        for val in dat:
-            if isinstance(val,list):
-                csv_out += f"\"{"|".join(val)}\";"
-            elif val is None:
-                csv_out += f"\"\";"
-            else:
-                csv_out += f"\"{escape(val)}\";"
-        csv_out += "\n"
+    fields = []
+    fields=list(response.json().get("results")[0].keys()) # Handle exception when no result is found; Relies on having all keys set on entry 0
+    fields.remove("openfda")
 
-    response = Response(csv_out, content_type="text/csv")
-    response.headers["Content-Disposition"] = "attachment; filename=out.csv"
-    return response
+    results = response.json().get("results")
+
+    for row_index,row in enumerate(results):
+        row.pop("openfda", None)
+        for col_index, value in enumerate(fields):
+            if isinstance(value,list):
+                results[row_index][col_index] = "|".join(value)
+    output = io.StringIO()
+    writer = csv.DictWriter(output, quoting=csv.QUOTE_NONNUMERIC, fieldnames=fields)
+    print("DATA")
+    print(results)
+
+    mem = io.BytesIO()
+    mem.write(output.getvalue().encode())
+
+    mem.seek(0)
+    
+
+    writer.writeheader()
+    writer.writerows(results)
+    output.close()
+    return send_file(
+        mem,
+        as_attachment=True,
+        download_name='test.csv',
+        mimetype='text/csv'
+    )
